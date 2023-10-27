c9d760e07495a18b25c30b01ec908f21b040f3ae
Robert Košir <robikoser@gmail.com>
2023-10-27 15:04:08 +0200

    Add Email content

--- api/crashes/helpers/email_helper.py
+++ api/crashes/helpers/email_helper.py
@@ -9,9 +9,25 @@ from api.crashes.serializers import CrashJSONSerializer
 
 def send_pdf(crash, recipients):
     pdf = crash.pdf
+
+    cars = crash.cars.all()
+    body = _("""
+Spoštovani,
+
+
+V priponki vam pošiljamo potrjen evropski obrazec o prometni nezgodi, v kateri sta bili
+udeleženi vozili z registrsko oznako %(car1)s in %(car2)s
+
+
+Želimo vam, da boste posledice nezgode čim prej uspešno uredili. 
+
+Lep pozdrav,
+Vaš Aksi tim
+""") % {'car1': cars[0].registration_plate, 'car2': cars[1].registration_plate}
+
     email = EmailMessage(
         subject=_("Crash confirmed"),
-        body="Content",
+        body=body,
         from_email=settings.DEFAULT_FROM_EMAIL,
         to=recipients
     )
